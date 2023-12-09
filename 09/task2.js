075fa93accf6548c12bfc10e22b9df7f159ec87f
Adam Scott <adam.scott@kognic.com>
2023-12-09 13:14:03 +0100

    d9

---
+++ 09/task2.js
@@ -0,0 +1,68 @@
+const { CreateLineReader } = require("../_util/LineReader");
+
+const inputLines = [];
+
+const getDerivataArray = (arr) => {
+  const nextArr = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    nextArr.push(arr[i + 1] - arr[i]);
+  }
+  return nextArr;
+};
+
+const getNextValue = (arr) => {
+  const pyramid = [arr];
+
+  // fill base
+  while (true) {
+    const next = getDerivataArray(pyramid[pyramid.length - 1]);
+    if (next.every((e) => e === 0)) {
+      next.push(0);
+      pyramid.push(next);
+      break;
+    }
+    pyramid.push(next);
+  }
+  pyramid.reverse();
+
+  pyramid.forEach((pLine, i) => {
+    if (i !== pyramid.length - 1) {
+      pyramid[i + 1].push(
+        pyramid[i + 1][pyramid[i + 1].length - 1] + pLine[pLine.length - 1]
+      );
+    }
+  });
+
+  const lastVal =
+    pyramid[pyramid.length - 1][pyramid[pyramid.length - 1].length - 1];
+  return lastVal;
+};
+
+const getNextValueSum = () => {
+  let sum = 0;
+  // console.log("inputLines", inputLines);
+  inputLines.forEach((inputLine) => {
+    sum += getNextValue(inputLine);
+  });
+  return sum;
+};
+
+const run = async (filename = "./sample.txt") => {
+  const lr = CreateLineReader(filename);
+  for await (const line of lr) {
+    inputLines.push(
+      line
+        .trim()
+        .split(" ")
+        .reverse()
+        .map((v) => parseInt(v))
+    );
+    // do stuff
+  }
+  const answer = getNextValueSum();
+  return answer;
+};
+
+module.exports = {
+  RunTask: (filename) => run(filename),
+};
--- 09/test.test.js
+++ 09/test.test.js
@@ -1,12 +1,12 @@
-const { RunTask } = require("./task1");
+const { RunTask } = require("./task2");
 
-const expectedOutput = 46;
+const expectedOutput = 2;
 
-const day = "0X";
+const day = "09";
 
-test("0" + day + " - task1", async () => {
-  // const answer = await RunTask("./" + day + "/sample.txt");
+test("0" + day + " - task2", async () => {
+  const answer = await RunTask("./" + day + "/input.txt");
 
-  expect(1).toBe(1);
-  // expect(answer).toBe(expectedOutput);
+  // expect(1).toBe(1);
+  expect(answer).toBe(expectedOutput);
 });
